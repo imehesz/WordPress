e0b6ebe47f98e0283d3968671f11291b8f54e3b7
Imre Mehesz <imehesz@gmail.com>
2014-09-02 03:36:41 +0400

    added big gray footer

--- wp-content/themes/bizzbuzztheme/footer.php
+++ wp-content/themes/bizzbuzztheme/footer.php
@@ -1,3 +1,4 @@
+    <!--
     <div class="dmbs-footer">
         <?php
             global $dm_settings;
@@ -9,8 +10,33 @@
 
         <?php get_template_part('template-part', 'footernav'); ?>
     </div>
+    -->
 
 </div>
+
+<section id="footer" class="section footer">
+  <div class="container">
+    <div class="row text-center copyright">
+      <div class="col-sm-12">
+        <p>
+          <hr />
+        </p>
+        <p>
+          <a href="https://www.facebook.com/bizzbuzzcomics">Facebook</a> -
+          <a href="https://twitter.com/bizzbuzzcomics">Twitter</a> -
+          <a href="http://www.pinterest.com/bizzbuzzcomics/">Pinterest</a>
+        </p>
+        <p>
+          Copyright &copy; 2014 <a href="http://bizzbuzzcomics.com">Bizz Buzz Comics</a> -
+          <a href="https://docs.google.com/document/d/1uc_juL24emg2E8n9c7htYX5FN3ihMG-g_yXvrPuT4R8">Terms and Conditions</a>
+        </p>
+        <p>
+          <a href="http://bizzbuzzcomics.com" title="Bizz Buzz Comics.com"><img src="https://43d4888fbfad7eaa47cb1338f5932a464633e963.googledrive.com/host/0B55OYxnBow_9N3EzTXNRQzZnYjg/bizzbuzz-logo-08142.png"></div></a>
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
 <!-- end main container -->
 
 <?php wp_footer(); ?>
