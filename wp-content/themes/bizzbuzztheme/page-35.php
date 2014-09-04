75122ba1d448258f47f349ec61c477280c27cc7c
Imre Mehesz <imehesz@gmail.com>
2014-09-05 03:53:39 +0400

    finally finished carousel ... phew

--- wp-content/themes/bizzbuzztheme/page-35.php
+++ wp-content/themes/bizzbuzztheme/page-35.php
@@ -12,8 +12,6 @@
     'meta_key'   => 'ComicStatus',
   );
 
-  // The Query
-  $the_query = new WP_Query( $args );
 ?>
 
 <!-- start content container -->
@@ -23,39 +21,41 @@
     <?php get_sidebar( 'left' ); ?>
 
     <div class="col-md-<?php devdmbootstrap3_main_content_width(); ?> dmbs-main">
-      <?php if ( $the_query->have_posts() ) : ?>
-        <div id="buzzCarousel" class="carousel slide" data-ride="carousel">
+      <p></p>
+      <div id="buzzCarousel" class="carousel slide" data-ride="carousel">
 
+        <?php $the_query2 = new WP_Query( $args ); if ( $the_query2->have_posts() ) : ?>
           <!-- Indicators -->
-          <?php /*
           <ol class="carousel-indicators">
-            <?php $cnt=0; while ( $the_query->have_posts() ): ?>
-            <li slide-to="<?php echo $cnt; ?>" class="<?php if($cnt==0) echo "active"; ?>"></li>
+            <?php $cnt=0; while ( $the_query2->have_posts() ): ?>
+              <?php $the_query2->the_post(); ?>
+              <li slide-to="<?php echo $cnt; ?>" class="<?php if($cnt==0) echo "active"; ?>"></li>
             <?php $cnt++; endwhile; wp_reset_postdata(); ?>
           </ol>
-          */ ?>
-
-          <?php $the_query2 = new WP_Query( $args ); $cnt=0; while ( $the_query2->have_posts() ): ?>
-            <?php $the_query2->the_post(); $cp = new ComicParser($post); ?>
-            <div class="carousel-inner">
-              <div class="item <?php if($cnt==$the_query2->post_count-1) echo "active"; ?>">
-                <img src="<?php echo $cp->getCover(); ?>" alt="<?php echo $cp->getTitle(); ?>">
-                  <div class="container">
-                    <div class="carousel-caption">
-                      <h1><?php echo $cp->getTitle(); ?></h1>
-                      <p><?php echo $cp->getExcerpt(); ?></p>
-                      <p><a class="btn btn-lg btn-success" href="<?php echo get_permalink($cp->getId()); ?>" role="button">Check it out!</a></p>
+        <?php endif; ?>
+  
+        <?php $the_query = new WP_Query( $args ); if ( $the_query->have_posts() ) : ?>
+            <div class="carousel-inner" style="height:500px;">
+              <?php $cnt=0; while ( $the_query->have_posts() ): ?>
+                <?php $the_query->the_post(); $cp = new ComicParser($post); ?>
+                  <div class="item <?php if($cnt==0) echo "active"; ?>">
+                    <img src="<?php echo $cp->getCover(); ?>" alt="<?php echo $cp->getTitle(); ?>">
+                    <div class="container">
+                      <div class="carousel-caption">
+                        <h1><?php echo $cp->getTitle(); ?></h1>
+                        <p><?php echo $cp->getExcerpt(); ?></p>
+                        <p><a class="btn btn-lg btn-success" href="<?php echo get_permalink($cp->getId()); ?>" role="button">Check it out!</a></p>
+                      </div>
                     </div>
                   </div>
-              </div>
+              <?php $cnt++; endwhile ?>
             </div>
-          <?php $cnt++; endwhile ?>
-
-          <a class="left carousel-control" onclick="return false;" href="#buzzCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-          <a class="right carousel-control" onclick="return false;" href="#buzzCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
- 
-        </div>
-      <?php endif; wp_reset_postdata(); ?>
+    
+            <a class="left carousel-control" onclick="return false;" href="#buzzCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+            <a class="right carousel-control" onclick="return false;" href="#buzzCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+   
+        <?php endif; wp_reset_postdata(); ?>
+      </div>
     </div>
 
 </div>
