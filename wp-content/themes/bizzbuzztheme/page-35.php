2b937c069bbfbc78817293dc84b1ea1e70eefea3
Imre Mehesz <imehesz@gmail.com>
2014-09-05 04:28:07 +0400

    finished home-page for now

--- wp-content/themes/bizzbuzztheme/page-35.php
+++ wp-content/themes/bizzbuzztheme/page-35.php
@@ -12,6 +12,11 @@
     'meta_key'   => 'ComicStatus',
   );
 
+  $randArgs = array(
+    'post_type'  => "comics",
+    "orderby" => "rand",
+    "posts_per_page" => "4"
+  );
 ?>
 
 <!-- start content container -->
@@ -56,8 +61,53 @@
    
         <?php endif; wp_reset_postdata(); ?>
       </div>
-    </div>
+      <p></p>
+      <div class="row text_center">
+        <div class="col-md-6">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- bizzbuzz-read-top -->
+        <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-1319358860215477"
+        data-ad-slot="6070069179"
+        data-ad-format="auto"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        </div>
+        <div class="col-md-6">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- bizzbuzz-read-top -->
+        <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-1319358860215477"
+        data-ad-slot="6070069179"
+        data-ad-format="auto"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        </div>
 
+      </div>
+      <p><br /></p>
+      <p></p>
+      <?php $the_query3 = new WP_Query( $randArgs ); if ( $the_query3->have_posts() ) : ?>
+        <div class="row text-center random-four">
+        <?php $cnt=0; while ( $the_query3->have_posts() ): ?>
+          <?php $the_query3->the_post(); $cp = new ComicParser($post); ?>
+            <div class="col-lg-6 col-md-6 portfolio-item">
+              <div class="random-four-img-wrapper">
+                <a href="<?php echo get_permalink($cp->getId()); ?>">
+                  <img class="img-responsive" src="<?php echo $cp->getThumbnail(); ?>">
+                </a>
+              </div>
+              <h3><a href="<?php echo get_permalink($cp->getId()); ?>"><?php echo $cp->getTitle(); ?></a></h3>
+              <p><?php echo $cp->getExcerpt(); ?></p>
+            </div>
+        <?php endwhile; ?>
+        </div>
+      <?php endif; wp_reset_postdata(); ?>
+    </div>
 </div>
 <!-- end content container -->
 
